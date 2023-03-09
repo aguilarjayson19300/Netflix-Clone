d77af30944fa6ca4f5d84cc8d1133f4602167785
aguilarjayson19300 <aguilarjayson19300@gmail.com>
2023-03-09 15:58:44 +0800

    remove unused code

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar';
 import Featured from '../components/Featured';
+import List from '../components/List';
 import '../App.css';
 import '../style/home.css';
 import '../style/font.css';
@@ -11,31 +12,12 @@ export const Home = () => {
     <div className='home'>
       <Navbar />
       <Featured />
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-       <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-            An extreme fan of Kung Fu, Po dreams of one day becoming a master 
-            worthy of fighting alongside the Five but his girth and clumsiness 
-            makes this dream simply a dream and his kung fu talents reside only 
-            within his knowledge of moves and artifacts.
-          
+      <List/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
     </div>
-
   )
 }
 
