c1071a9b0d738a35f928a73145f632f9a400477b
aguilarjayson19300 <aguilarjayson19300@gmail.com>
2023-03-09 15:53:51 +0800

    create interface

--- src/data.tsx
+++ src/data.tsx
@@ -1,4 +1,9 @@
-export const languages = [
+interface languageDto {
+    id: number,
+    language: string
+}
+
+export const languages: languageDto[] = [
     { id: 1, language: "Filipino" },
     { id: 2, language: "English" },
     { id: 3, language: "Portuges" }
