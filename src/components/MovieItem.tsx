bf1a1993207c5257b73d87064f951d6e22f8ff01
aguilarjayson19300 <aguilarjayson19300@gmail.com>
2023-03-13 10:30:29 +0800

    fix hover issues

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import '../style/listOfItems.css';
 
 interface ListItemsProps {
-  movie: {name: string, imgSrc: string, vidSrc: string};
+  movie: {name: string, imgSrc: string, vidSrc: string, top: string};
   index: number;
 }
 
@@ -17,26 +17,27 @@ interface ListItemsProps {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
+      <p className='movieTitle'>{movie.name}</p>
       <img src={movie.imgSrc} className='movieCover'/>
 
       {isHovered && (
-        <div className='itemInfo'>
+        <div className='itemInfoList'>
           <video src={trailer} autoPlay={true} muted loop />
-          <div className='movieInfo'>
-              <div className='icons'>
+          <div className='movieInfoList'>
+              <div className='iconsList'>
                 <span className="material-symbols-rounded"> play_circle </span>
                 <span className="material-symbols-rounded"> add_circle </span>
                 <span className="material-symbols-rounded"> recommend </span>
                 <div className='moreMovieDetails'><span className="material-symbols-rounded arrow-down"> expand_circle_down </span></div>
               </div>
 
-                <div className='itemInfoTop'>
-                  <span className='match'>98% Match</span>
+                <div className='itemInfoTopList'>
+                  <span className='matchList'>98% Match</span>
                   <span className="material-symbols-rounded"> looks_3 </span>
                   <span>16 Episodes</span>
                   <span className="material-symbols-rounded"> hd </span>
                 </div>
-                <div className='genre'>
+                <div className='genreList'>
                   <span> Kilig . Romantic . Youth</span>
                 </div>
           </div>
--- src/components/MovieModal.tsx
+++ src/components/MovieModal.tsx
@@ -8,33 +8,89 @@ interface MovieModalProps {
 export const MovieModal = ({
     modalStatus
 }: MovieModalProps) => {
-
     return (
         <>
-            <div className="modal">
-                <div className="modal-content">
-                    <span className="material-symbols-rounded close" onClick={modalStatus}> cancel </span>
-                </div>
-                <img src='../movies/kufupanda.png' alt='Ku Fu Panda' />
-                <div className='MovieDetails'>
-                    <div className='icons'>
-                        <span className="material-symbols-rounded"> play_circle </span>
-                        <span className="material-symbols-rounded"> add_circle </span>
-                        <span className="material-symbols-rounded"> recommend </span>
+            <div className='modalContainer'>
+                <div onClick={modalStatus} className="overlay"></div>
+                <div className="modal">
+                    <img className='imgModal' src='../movies/kufupanda.png' alt='Ku Fu Panda' />
+                    <div className='MovieDetailsModal'>
+                        <div className='iconsModal'>
+                            <span className="material-symbols-rounded playIcon"> play_circle </span>
+                            <span className="material-symbols-rounded"> add_circle </span>
+                            <span className="material-symbols-rounded"> recommend </span>
+                        </div>
                     </div>
-                    <div className='itemInfoTop'>
-                        <span className='match'>98% Match</span>
-                        <span className="material-symbols-rounded"> looks_3 </span>
-                        <span>16 Episodes</span>
-                        <span className="material-symbols-rounded"> hd </span>
+                    <video autoPlay muted loop>
+                        <source src="/movies/kufupandahover.mp4" type="video/mp4" />
+                    </video>
+                    
+                    <div className='movieInfo'>
+                        <div className='firstInfo'>
+                            <div className='rating'>
+                                <span className='match'>98% Match</span>
+                                <span className="material-symbols-rounded match"> looks_3 </span>
+                                <span>16 Episodes</span>
+                                <span className="material-symbols-rounded match"> hd </span>
+                            </div>
+                            <div className='plotModal'>
+                                An extreme fan of Kung Fu, Po dreams of one day becoming a master worthy of fighting alongside the Five but his girth and clumsiness makes this dream simply a dream and his kung fu talents reside only within his knowledge of moves and artifacts.
+                            </div>
+                        </div>
+                        <div className='secondInfo'>
+                            <p className='gray'>Cast:</p>
+                            <p>Kim Hee-ai, Park Hae-joon, Han so-hee, more</p>
+                            <p className='gray'>Genre:</p>
+                            <p>Anime, Action Pantacy</p>
+                            <p className='gray'>This show is:</p>
+                            <p>Dark, Action and Dramatic</p>
+                        </div>
                     </div>
+                    <div className='labelModal'>
+                        <h3>Epidodes</h3>
+                        <h3>Kunf Fu Panda</h3>
+                    </div>
+                    <div className='episodes'>
+                        <div className='episodesNumber'>1</div>
+                        <div className='episodeImg'><img src='../movies/kufupandatm.jpg'/></div>
+                        <div className='time'>
+                            <div className='labelPlot'>
+                                <span className='number'>Episode 1</span>
+                                <span className='minutes'>88 mins</span>
+                            </div>
+                            <div className='episodesPlot'>
+                                An extreme fan of Kung Fu, Po dreams of one day becoming a master worthy of fighting alongside the Five but his girth and clumsiness makes this dream simply a dream and his kung fu talents reside only within his knowledge of moves and artifacts.
+                            </div>
+                        </div>
+                    </div>
+                    <div className='episodes'>
+                        <div className='episodesNumber'>2</div>
+                        <div className='episodeImg'><img src='../movies/kufupandatm.jpg'/></div>
+                        <div className='time'>
+                            <div className='labelPlot'>
+                                <span className='number'>Episode 2</span>
+                                <span className='minutes'>80 mins</span>
+                            </div>
+                            <div className='episodesPlot'>
+                                An extreme fan of Kung Fu, Po dreams of one day becoming a master worthy of fighting alongside the Five but his girth and clumsiness makes this dream simply a dream and his kung fu talents reside only within his knowledge of moves and artifacts.
+                            </div>
+                        </div>
+                    </div>
+                    <div className='episodes'>
+                        <div className='episodesNumber'>3</div>
+                        <div className='episodeImg'><img src='../movies/kufupandatm.jpg'/></div>
+                        <div className='time'>
+                            <div className='labelPlot'>
+                                <span className='number'>Episode 3</span>
+                                <span className='minutes'>87 mins</span>
+                            </div>
+                            <div className='episodesPlot'>
+                                An extreme fan of Kung Fu, Po dreams of one day becoming a master worthy of fighting alongside the Five but his girth and clumsiness makes this dream simply a dream and his kung fu talents reside only within his knowledge of moves and artifacts.
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="modal-content">
-                    <span className="material-symbols-rounded close" onClick={modalStatus}> cancel </span>
-                </div>
-                <video autoPlay muted loop>
-                    <source src="/movies/kufupanda.mp4" type="video/mp4" />
-                </video>
             </div>
         </>
     );
