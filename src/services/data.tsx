64272563216805b7ef527a65f3100c6bc940c304
aguilarjayson19300 <aguilarjayson19300@gmail.com>
2023-03-13 10:30:53 +0800

    add data videos and name

--- src/services/data.tsx
+++ src/services/data.tsx
@@ -1,37 +1,52 @@
+interface moviesByCategories {
+    category: string,
+    movies: { name: string, imgSrc: string, vidSrc: string, top: string}[],
+  }
+
 export const moviesByCategories = [
     {
         category: 'Continue Watching',
         movies: [
-            { name: 'Idian', imgSrc: './movies/movie1 (1).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Scream ',imgSrc: './movies/movie1 (2).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (3).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (4).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (5).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (6).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (7).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (8).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (9).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (10).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (11).jpg', vidSrc: '/movies/kufupanda.mp4' }
+            { top: '1', name: 'Moana', imgSrc: './movies/movie1 (1).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '2', name: 'Iskandalo ',imgSrc: './movies/movie1 (2).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '3', name: 'Jowable', imgSrc: './movies/movie1 (3).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '4', name: 'NUUK', imgSrc: './movies/movie1 (4).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '5', name: 'Oppa', imgSrc: './movies/movie1 (5).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '7', name: 'Exit', imgSrc: './movies/movie1 (7).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '8', name: 'Oblivion', imgSrc: './movies/movie1 (8).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '9', name: 'Petter Rabbit', imgSrc: './movies/movie1 (9).jpg', vidSrc: '/movies/moana.mp4'},
+            { top: '10', name: 'Godzilla', imgSrc: './movies/movie1 (10).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '11', name: '1917', imgSrc: './movies/movie1 (11).jpg', vidSrc: '/movies/moana.mp4' }
         ]
     },
     {
         category: 'Top 10',
         movies: [
-            { name: 'Argo', imgSrc: './movies/movie1 (6).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (7).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (8).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (9).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (10).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (11).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Idian', imgSrc: './movies/movie1 (1).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Scream ',imgSrc: './movies/movie1 (2).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (3).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (4).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (5).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (6).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (7).jpg', vidSrc: '/movies/kufupanda.mp4' },
-            { name: 'Argo', imgSrc: './movies/movie1 (8).jpg', vidSrc: '/movies/kufupanda.mp4' }
+            { top: '7', name: 'Exit', imgSrc: './movies/movie1 (7).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '8', name: 'Oblivion', imgSrc: './movies/movie1 (8).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '9', name: 'Petter Rabbit', imgSrc: './movies/movie1 (9).jpg', vidSrc: '/movies/moana.mp4'},
+            { top: '10', name: 'Godzilla', imgSrc: './movies/movie1 (10).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '11', name: '1917', imgSrc: './movies/movie1 (11).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '1', name: 'Moana', imgSrc: './movies/movie1 (1).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '2', name: 'Iskandalo ',imgSrc: './movies/movie1 (2).jpg', vidSrc: '/movies/iskandalo' },
+            { top: '3', name: 'Jowable', imgSrc: './movies/movie1 (3).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '4', name: 'NUUK', imgSrc: './movies/movie1 (4).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '5', name: 'Oppa', imgSrc: './movies/movie1 (5).jpg', vidSrc: '/movies/moana.mp4' }
+        ]
+    },
+    {
+        category: 'TV Shows',
+        movies: [
+            { top: '1', name: 'Moana', imgSrc: './movies/movie1 (1).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '2', name: 'Iskandalo ',imgSrc: './movies/movie1 (2).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '3', name: 'Jowable', imgSrc: './movies/movie1 (3).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '4', name: 'NUUK', imgSrc: './movies/movie1 (4).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '5', name: 'Oppa', imgSrc: './movies/movie1 (5).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '7', name: 'Exit', imgSrc: './movies/movie1 (7).jpg', vidSrc: '/movies/moana.mp4' },
+            { top: '8', name: 'Oblivion', imgSrc: './movies/movie1 (8).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '9', name: 'Petter Rabbit', imgSrc: './movies/movie1 (9).jpg', vidSrc: '/movies/moana.mp4'},
+            { top: '10', name: 'Godzilla', imgSrc: './movies/movie1 (10).jpg', vidSrc: '/movies/iskandalo.mp4' },
+            { top: '11', name: '1917', imgSrc: './movies/movie1 (11).jpg', vidSrc: '/movies/moana.mp4' }
         ]
     }
 ]
